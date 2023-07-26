ba283e95419d371623a433d9f8d3f9646fff1bfa
Prashant <ummadi1994@gmail.com>
2023-07-26 16:43:40 +0530

    Updated

--- Functions.sh
+++ Functions.sh
@@ -3,7 +3,9 @@
 # Define a function called "greet"
 function greet() {
     local name=$1  # Get the first argument and store it in the "name" variable
+    local place=$2
     echo "Hello, $name! How are you today?"
+    echo "Hello, $place! Whats your Location?"
 }
 
 # Call the function and pass a name as an argument
